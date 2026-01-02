332ea9dadd401efa13ce4046861107c72e0eaed9
baillodev <mamadoubailodiallo952@gmail.com>
2026-01-02 21:52:16 +0000

    style: implement grid background with radial mask | chore: remove default favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,17 +12,33 @@ export default function Home() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center gap-20 h-svh">
-      <NumberBlocks active={active} />
-      <h2 
-        className={clsx(
-          "text-[#252525] text-4xl text-center transition duration-500 cursor-pointer after:content-[''] after:transition after:duration-500",
-          active && "text-white [text-shadow:0_0_20px_#fff,0_0_50px_#fff] after:text-[#32ed4e] after:[text-shadow:0_0_20px_#32ed4e,0_0_50px_#32ed4e]"
-        )}
-        onClick={toggleActive}
-      >
-        Happy New Year <span className="text-[#32ed4e]">2026</span>
-      </h2>
+    <div className="relative h-svh w-full bg-[#111] overflow-hidden">
+      
+      <div 
+        className="absolute inset-0 z-0 pointer-events-none"
+        style={{
+          backgroundImage: `
+            linear-gradient(to right, rgba(255, 255, 255, 0.05) 1px, transparent 1px),
+            linear-gradient(to bottom, rgba(255, 255, 255, 0.05) 1px, transparent 1px)
+          `,
+          backgroundSize: '40px 40px',
+          maskImage: 'radial-gradient(circle at center, black, transparent 80%)',
+        }}
+      />
+
+      <div className="relative h-full z-10 flex flex-col items-center justify-center gap-12.5">
+        <NumberBlocks active={active} />
+        
+        <h2 
+          className={clsx(
+            "text-[#252525] text-3xl text-center transition duration-500 cursor-pointer select-none after:content-[''] after:transition after:duration-500",
+            active && "text-white [text-shadow:0_0_20px_#fff,0_0_50px_#fff] after:text-[#32ed4e] after:[text-shadow:0_0_20px_#32ed4e,0_0_50px_#32ed4e]"
+          )}
+          onClick={toggleActive}
+        >
+          Happy New Year <span className="text-[#32ed4e]">2026</span>
+        </h2>
+      </div>
     </div>
   );
 }
--- src/components/blocks/number-blocks/number-block.tsx
+++ src/components/blocks/number-blocks/number-block.tsx
@@ -9,7 +9,7 @@ export function NumberBlock({
     return (
         <div
             style={{"--j": index} as React.CSSProperties}
-            className="relative w-25 h-25 transform-3d transition ease-in-out duration-[2.5s] delay-[calc(0.25s*var(--j))] before:content-[''] before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-[#373737] before:rotate-y-90 before:-translate-x-12.5 last:before:bg-[#11a728] [.active>&]:rotate-x-[calc(-360deg*1)] [.active>&]:last:rotate-x-[calc(-630deg*1)]"
+            className="relative w-20 h-20 transform-3d transition ease-in-out duration-[2.5s] delay-[calc(0.25s*var(--j))] before:content-[''] before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-[#373737] before:rotate-y-90 before:-translate-x-10 last:before:bg-[#11a728] [.active>&]:rotate-x-[calc(-360deg*1)] [.active>&]:last:rotate-x-[calc(-630deg*1)]"
         >
             {values.map((value, i) => {
                 return (
@@ -17,7 +17,7 @@ export function NumberBlock({
                         key={i}
                         style={{ "--i": i } as React.CSSProperties}
                         className={clsx(
-                            "absolute top-0 left-0 w-full h-full font-light text-7xl flex justify-center items-center transform-3d transform-[rotateX(calc(90deg*var(--i)))_translateZ(50px)]",
+                            "absolute top-0 left-0 w-full h-full font-light text-6xl flex justify-center items-center transform-3d transform-[rotateX(calc(90deg*var(--i)))_translateZ(40px)]",
                             index === totalBlocks - 1
                                 ? "bg-linear-to-b from-[#29c040] to-[#32ed4e] before:bg-[#11a728]"
                                 : "bg-linear-to-b from-[#434343] to-[#535353]"
